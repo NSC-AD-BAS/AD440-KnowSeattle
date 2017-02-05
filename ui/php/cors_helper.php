fd481e9bb1f861c54e4808a15392f8b5f6750803
Jeremy Duke <jeremy@tetrapod.io>
2017-02-04 22:28:01 -0800

    Add PHP helper for WalkScore calls (and anyone else who doesn't allow Cross-Origin.

---
+++ ui/php/cors_helper.php
@@ -0,0 +1,40 @@
+<?php
+    $func = $_GET["f"];
+
+    //Add other helper function calls here
+    if ($func == "walkscore") {
+        getWalkScore();
+    }
+
+    //helper function implementations
+    function getWalkScore() {
+        $loc = $_GET["loc"];    //Get the location object
+        //print_r($loc);        //See the fields in the object
+        $lat = $loc['lat']; $lon = $loc['lng'];
+        $key = "10284fa9f60a76d6175a7fb5d834ad20";
+        $url = "http://api.walkscore.com/score?format=json&lat=". $lat ."&lon=" . $lon . "&transit=1&bike=1&wsapikey=" . $key;
+
+        echo do_curl($url);
+    }
+
+    //other helper functions go here...
+    //function getCORSData() {...}
+
+    //Utility functions
+    function do_curl($url) {
+        //  Initiate curl
+        $ch = curl_init();
+        // Disable SSL verification
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // Will return the response, if false it print the response
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // Set the url
+        curl_setopt($ch, CURLOPT_URL,$url);
+        // Execute
+        $result=curl_exec($ch);
+        // Closing
+        curl_close($ch);
+
+        return $result;
+    }
+?>
