c8469c63ae49613fcdd2893381ca8b20900fd032
Joe Mclaughlin <jomclis@gmail.com>
2017-02-08 11:46:05 -0800

    Styles tile for Concert data

--- webroot/js/pages/concerts.js
+++ webroot/js/pages/concerts.js
@@ -5,8 +5,8 @@
             var zip = loc.zip;
             var radius = 0;//parseInt(document.getElementById('rad').value);
 	    
-	    var apikey = "6mnky94spc5h9u8n3fjxq6t7"; //joes
-	    //var apikey = "k5dywsuqf9vaexvg5xczcspf"; //brandons	
+	    //var apikey = "6mnky94spc5h9u8n3fjxq6t7"; //joes
+	    var apikey = "k5dywsuqf9vaexvg5xczcspf"; //brandons	
 
 			$.getJSON("http://api.jambase.com/events?zipCode=" + zip +"&radius=" + radius + "&page=0&api_key=" + apikey, 
     			function(data) {
@@ -17,7 +17,11 @@
 
         		for (i = 0; i < 15; i++)//data.Info.TotalResults for all results, currently returns just 50
         		{
-        		json += "<div class='tile'><ul><li>" + data.Events[i].Artists[0].Name + " </li><li> at " + data.Events[i].Venue.Name + "</li><li> on " + data.Events[i].Date + "</li></ul></div>";
+                    
+                    var date = data.Events[i].Date;
+                    var split = date.split("T",2);
+                    
+        		json += "<div class='tile' ><b>Artists:</b><br /><div style='font-size: 15px'>" +  data.Events[i].Artists[0].Name + "</div>  <b>Venue:</b><br /><div style='font-size: 15px'>" + data.Events[i].Venue.Name + "</div> <b>Day:</b><br /><div style='font-size: 15px'> " + split[0] + "</div> <b>Time:</b><br /><div style='font-size: 15px'> " + split[1] + "</div></div>";
 				// shows 1st artists, venue, date
         			//document.write("<br>");
 
