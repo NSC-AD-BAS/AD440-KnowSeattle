68da831b3339db121985eec3b5628617d2762e1c
Joe Mclaughlin <jomclis@gmail.com>
2017-02-08 10:40:43 -0800

    Adds tile look to results and adds required mention of jambase for api use

--- webroot/js/pages/concerts.js
+++ webroot/js/pages/concerts.js
@@ -5,24 +5,24 @@
             var zip = loc.zip;
             var radius = 0;//parseInt(document.getElementById('rad').value);
 	    
-	    //var apikey = "6mnky94spc5h9u8n3fjxq6t7"; //joes
-	    var apikey = "k5dywsuqf9vaexvg5xczcspf"; //brandons	
+	    var apikey = "6mnky94spc5h9u8n3fjxq6t7"; //joes
+	    //var apikey = "k5dywsuqf9vaexvg5xczcspf"; //brandons	
 
 			$.getJSON("http://api.jambase.com/events?zipCode=" + zip +"&radius=" + radius + "&page=0&api_key=" + apikey, 
     			function(data) {
         		//console.log(data.Events);
 		
    
-        		var json;
+        		var json = "<div style='display: flex; flex-wrap: wrap'>";
 
-        		for (i = 0; i < 10; i++)//data.Info.TotalResults for all results, currently returns just 50
+        		for (i = 0; i < 15; i++)//data.Info.TotalResults for all results, currently returns just 50
         		{
-        		json += "<ul><li>" + data.Events[i].Artists[0].Name + " </li><li> at " + data.Events[i].Venue.Name + "</li><li> on " + data.Events[i].Date + "</li></ul>";
+        		json += "<div class='tile'><ul><li>" + data.Events[i].Artists[0].Name + " </li><li> at " + data.Events[i].Venue.Name + "</li><li> on " + data.Events[i].Date + "</li></ul></div>";
 				// shows 1st artists, venue, date
         			//document.write("<br>");
 
         				}
-
+                json += "</div><br ><br /><br /><a href=http://www.jambase.com>Results powered by JamBase™</a>";
         			
 
       success(json);
