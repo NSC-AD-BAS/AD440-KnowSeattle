cfb8962226c95fd097c4d20a4abd6840d1415cac
Jeremy Duke <jeremy@tetrapod.io>
2017-03-05 15:56:53 -0800

    Generic cors_helper implementation in javascript utils, move walkscore over to JS cors.

--- webroot/js/pages/walkscore.js
+++ webroot/js/pages/walkscore.js
@@ -1,50 +1,17 @@
 function getWalkScoreData(loc, success, error) {
-   var url="php/cors_helper.php?f=walkscore";
-   $.ajax({
-         url: url,
-         type:'GET',
-         data: {loc: loc}
-      }).done(function (data) {
-         var json = JSON.parse(data);
-         var summary = "<li>Walk: " + json.walkscore + "</li><li>Transit: " + json.transit.score + "</li><li>Bike: " + json.bike.score + "</li>";
-         success(summary);
-      }).fail(function() {
-         var summary = "Error getting WalkScore data";
-         console.error(summary);
-         error(summary);
-      });
-   }
-
-//The code below was taken from the page source for the walk-score API documentation and is currently unused.
-//We may eventually want to add in the infoIconHtml walk score to some of the pages.
-
-//to demonstrate all of our formatting options, we'll pass the json on to a series of display functions.
-//in practice, you'll only need one of these, and the ajax call could call it directly as it's onSuccess callback
-function displayWalkScores(jsonStr) {
-   displayWalkScore(jsonStr);
-}
-
-//show the walk score -- inserts walkscore html into the page.  Also needs CSS from top of file
-function displayWalkScore(jsonStr) {
-   var json=(jsonStr) ? eval('(' + jsonStr + ')') : ""; //if no response, bypass the eval
-
-   //if we got a score
-   if (json && json.status == 1) {
-      var htmlStr = '<a target="_blank" href="' + json.help_link + '"><img src="' + json.logo_url + '" /><span class="walkscore-scoretext">' + json.walkscore + '</span></a>';
-   }
-   //if no score was available
-   else if (json && json.status == 2) {
-      var htmlStr = '<a target="_blank" href="' + json.help_link + '"><img src="' + json.logo_url + '" /></a> <a href="' + json.ws_link + '"><span class="walkscore-noscoretext">Get Score</span></a>';
-   }
-   //if didn't even get a json response
-   else {
-      var htmlStr = '<a target="_blank" href="https://www.walkscore.com"><img src="//cdn2.walk.sc/2/images/api-logo.png" /> <span class="walkscore-noscoretext">Get Score</span></a> ';
-   }
-   var infoIconHtml = '<span id="ws_info"><a href="https://www.redfin.com/how-walk-score-works" target="_blank"><img src="//cdn2.walk.sc/2/images/api-more-info.gif" width="13" height="13"" /></a></span>';
-
-   //if you want to wrap extra tags around the html, can do that here before inserting into page element
-   htmlStr = '<p>' + htmlStr + infoIconHtml + '</p>';
-
-   //insert our new content into the container div:
-   $("#walkscore-div").html(htmlStr);
+   var key = "10284fa9f60a76d6175a7fb5d834ad20";      //TODO: This should be a node or mongo get query #Security
+   var url = "http://api.walkscore.com/score?format=json&lat=" + loc.lat + "&lon=" + loc.lng + "&transit=1&bike=1&wsapikey=" + key;
+   getCorsData({
+      method: 'GET',
+      url: url,
+      data: ""
+   }, function (data) {
+      var json = JSON.parse(data);
+      var summary = "<li>Walk: " + json.walkscore + "</li><li>Transit: " + json.transit.score + "</li><li>Bike: " + json.bike.score + "</li>";
+      success(summary);
+   }, function () {
+      var summary = "Error getting WalkScore data";
+      console.error(summary);
+      error(summary);
+   });
 }
---
+++ webroot/js/utils/cors_helper.js
@@ -0,0 +1,30 @@
+function getCorsData(jsonOpts, success, fail) {
+   var x = new XMLHttpRequest();  var msg = "";
+   x.open(jsonOpts.method, cors_api_url + jsonOpts.url);
+   x.onerror = function () {
+      console.log("An error occurred opening the request:");
+   };
+   try {
+      x.onload = function() {
+         return success(x.responseText);
+      };
+   } catch (err) {
+      msg = "An error occurred loading the response.  Error: " + err;
+      fail(msg);
+      console.error(msg);
+   }
+
+   //TODO: is this ever used?
+   if (/^POST/i.test(jsonOpts.method)) {
+      x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+   }
+
+   try {
+      x.setRequestHeader('x-requested-with', 'XMLHTTPREQUEST');
+      x.send(jsonOpts.data);
+   } catch (err) {
+      msg = "An error occurred sending the request.  Error: " + err;
+      console.error(msg);
+      console.error(cors_api_url + jsonOpts.url);
+   }
+}
