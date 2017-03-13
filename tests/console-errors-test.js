998b3a38c0154a53c3701d38f491402ace8c4aed
Kellan Nealy <gellinn@uw.edu>
2017-03-13 15:22:00 -0700

    changes to Travis CI test:
    -console-errors-test now displays console output from the webpage to your terminal
    
    make sure you run npm install again to get phantomjs (new dependency)
    
    run command:
    ./node_modules/phantomjs/bin/phantomjs tests/console-errors-test.js

--- tests/console-errors-test.js
+++ tests/console-errors-test.js
@@ -1,9 +1,9 @@
 /*
 * Author: Kellan Nealy
 * This test navigates the site and fails if there are console errors
-*/
+*
 var webdriver = require('selenium-webdriver'), driver;
- 
+
 driver = new webdriver.Builder().
   withCapabilities({
     'browserName': 'firefox',
@@ -23,4 +23,28 @@ driver.getTitle().then(function (title) {
     console.log("title is: " + title);
 });
 
-driver.quit();
+driver.quit();*/
+
+var url = "http://localhost:8888"
+var page = require('webpage').create();
+var t = Date.now();
+console.log('testing page at: ' + url);
+
+page.onConsoleMessage = function(msg) {
+    console.log('Page title is ' + msg);
+};
+page.open(url, function(status) {
+    if (status !== 'success') {
+        console.log('FAIL to load the address');
+    } else {
+        t = Date.now() - t;
+        console.log('Loading ' + url);
+        console.log('Loading time ' + t + ' msec');
+        //evaluate load time for the page
+        page.evaluate(function() {
+            console.log(document.title);
+        });
+    }
+    // Exit phantomjs after tests
+    phantom.exit();
+});
